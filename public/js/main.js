2e263409d6216e62141687321a538b164344b74c
Noureddine ESSID <noureddinesid@gmail.com>
2023-05-18 18:31:04 +0200

    Add controllers and templates

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+// // ~~~~~~~~~~~~~~~~~~~Transparency on menu when scroll
+var top_height = $(".jumbotron").offset().top;
+
+$(window).scroll(function (){
+// When scroll over the jumbotron the nav gets some transparency
+  if ($(window).scrollTop() > top_height) {
+    $("nav").css('opacity', 0.9);
+}
+// Otherwise full color
+  else {
+    $("nav").css('opacity', 1);
+  }
+});
