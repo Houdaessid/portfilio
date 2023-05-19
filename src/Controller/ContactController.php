50732737ff056a8f1cfaf3cd0ffcf31d96fe4124
Noureddine ESSID <noureddinesid@gmail.com>
2023-05-19 12:01:17 +0200

    Add some pages

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,15 +4,52 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use App\Entity\Contact;
+use App\Form\ContactType;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, ValidatorInterface $validator, MailerInterface $mailer): Response
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+          $errors = $validator->validate($contact);
+          if(count($errors) > 0){
+            $this->addFlash(
+              'errors',
+              $errors
+            );
+          }
+          else {
+            $email = (new Email())
+            ->from($contact->getEmail())
+            ->to($this->getParameter('contact_email'))
+            ->subject('Prise de contact de ' . $contact->getName())
+            ->text($contact->getMessage());
+            try {
+              $sentEmail = $mailer->send($email);
+              $this->addFlash(
+                'success',
+                'Votre message a bien été envoyé. Je reviendrai vers vous le plus rapidement possible.'
+              );
+            } catch (\Exception $e) {
+              $this->addFlash(
+                'danger',
+                "Un problème est survenu, votre message n'a pas pu être envoyé. Merci de réessayer."
+              );
+            }
+          }
+        }
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+          "form" => $form->createView()
         ]);
     }
 }
--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -5,13 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Project;
+use App\Form\ProjectType;
+use App\Repository\ProjectRepository;
 
 class ProjectsController extends AbstractController
 {
     #[Route('/projects', name: 'app_projects')]
-    public function index(): Response
+    public function index(ProjectRepository $projectRepository): Response
     {
         return $this->render('projects/index.html.twig', [
+            // 'projects' => $projectRepository->findBy([], ["position" => "ASC"]),
+            'projects' => [],
+        ]);
+    }
+
+    #[Route('/projects/new', name: 'project_new')]
+    public function new(): Response
+    {
+        return $this->render('projects/new.html.twig', [
             'controller_name' => 'ProjectsController',
         ]);
     }
